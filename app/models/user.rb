0d4fc1460778b4e6d505d3c4a83498eaaee58aed
PrashantYadav14 <prashantyadav.sjs@gmail.com>
2023-08-07 10:54:38 +0530

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-    
+  validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 25}
+  VALID_EMAIL_REGEX
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 100}
 end
