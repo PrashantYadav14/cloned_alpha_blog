789939017acc8c654be99780b29bbb0ec66553df
PrashantYadav14 <prashantyadav.sjs@gmail.com>
2023-08-07 13:27:57 +0530

    Add one to many association

---
+++ db/migrate/20230807062450_add_foreign_key_to_articles.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToArticles < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :articles, :user, foreign_key: true
+  end
+end
---
+++ db/migrate/20230807063846_remove_column_name_from_articles.rb
@@ -0,0 +1,5 @@
+class RemoveColumnNameFromArticles < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :articles, :username, :int
+  end
+end
