3ed368b3600ff723ceed33073ecb1914061f7cf9
PrashantYadav14 <prashantyadav.sjs@gmail.com>
2023-08-11 12:00:06 +0530

    Add user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @articles = @user.articles
   end
+  def index
+    @users = User.all
+  end
   def new
+    @flag=1
     @user = User.new
   end
   def edit
+    @flag=0
     @user = User.find(params[:id])
   end
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice]  = "Your account information was succeessfully updated"
-      redirect_to articles_path
+      redirect_to users_path
     else
       render 'edit', status: :unprocessable_entity
     end
@@ -22,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the alpha blog #{@user.username}! You have successfully signed up"
-      redirect_to articles_path
+      redirect_to users_path
     else
       render 'new', status: :unprocessable_entity
     end
