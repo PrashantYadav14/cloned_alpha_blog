43cf7306dab88a1a77fef1e11a434d8339d1646e
PrashantYadav14 <prashantyadav.sjs@gmail.com>
2023-08-14 14:56:29 +0530

    Delete user and its articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update,:destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   def index
@@ -12,10 +14,8 @@ class UsersController < ApplicationController
   end
   def edit
     @flag=0
-    @user = User.find(params[:id])
   end
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice]  = "Your account information was succeessfully updated"
       redirect_to users_path
@@ -26,15 +26,30 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the alpha blog #{@user.username}! You have successfully signed up"
       redirect_to users_path
     else
       render 'new', status: :unprocessable_entity
     end
   end
-
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Accounts and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end 
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account"
+      redirect_to @user
+    end
   end
 end
